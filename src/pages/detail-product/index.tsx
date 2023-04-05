e5851c97dbde8ac334f0b68cb4e8c32ef09f56e9
Michelle Penna <mp.design.apps@gmail.com>
2023-04-05 18:43:59 -0500

    feat(DetailProduct): add file detail products

---
+++ src/pages/detail-product/index.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "native-base";
+
+const DetailProductScreen = () => {
+
+  return (
+    <Box safeArea h="100%" justifyContent="center" alignItems="center">
+      <Text>Hola</Text>
+    </Box>
+  );
+};
+
+export default DetailProductScreen;
