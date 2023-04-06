9cfcb98c182c885f8c90ebdd9747a7584f277dc8
Michelle Penna <mp.design.apps@gmail.com>
2023-04-06 00:20:41 -0500

    feat(Product): logic product and filters

---
+++ src/pages/product/hooks/use-product.ts
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../../services/types/redux";
+import ProductActions from "../../../services/product/actions";
+
+type enumFilter = "all" | "won" | "redeemed";
+
+const useProduct = () => {
+  const dispatch = useAppDispatch();
+  const { success } = useAppSelector((state) => state.product);
+  const [total, setTotal] = useState(0);
+  const [filter, setFilter] = useState<enumFilter>("all");
+
+  useEffect(() => {
+    setTotal(
+      success.products.reduce((acum, current) => {
+        if (current.is_redemption) {
+          return acum - current.points;
+        } else {
+          return acum + current.points;
+        }
+      }, 0)
+    );
+  }, [success.products]);
+
+  useEffect(() => {
+    dispatch(ProductActions.getProducts());
+  }, []);
+
+  return {
+    products: success.products.filter((product) => {
+      if (filter === "all") {
+        return true;
+      }
+      if (filter === "won") {
+        return !product.is_redemption;
+      }
+      if (filter === "redeemed") {
+        return product.is_redemption;
+      }
+    }),
+    total,
+    setFilter,
+  };
+};
+
+export default useProduct;
