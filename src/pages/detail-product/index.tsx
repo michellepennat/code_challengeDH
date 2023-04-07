70a2d802fd1256278cffbda3a1a2da68e82e6a17
Michelle Penna <alejandrapenna15@gmail.com>
2023-04-07 12:06:52 -0500

    feat(detailProduct): add structure and data

--- src/pages/detail-product/index.tsx
+++ src/pages/detail-product/index.tsx
@@ -1,10 +1,35 @@
-import { Box, Text } from "native-base";
+import moment from "moment";
+import { Box, Button, Image, Text } from "native-base";
+import useDetailProduct from "./hooks";
+import "moment/locale/es";
+
+moment.locale("es");
 
 const DetailProductScreen = () => {
-
+  const { selectedProduct } = useDetailProduct();
+  // if (!selectedProduct) <>No</>;
   return (
     <Box safeArea h="100%" justifyContent="center" alignItems="center">
-      <Text>Hola</Text>
+      <Text>Hola {selectedProduct?.product}</Text>
+      <Image
+        size="100px"
+        borderRadius={10}
+        alt={selectedProduct?.product}
+        source={{
+          uri: selectedProduct?.image,
+        }}
+      />
+      <Text>Detalles del producto:</Text>
+      <Text>
+        Comprado el
+        {moment(selectedProduct?.createdAt).format("DD [de] MMMM, YYYY")}
+      </Text>
+      <Text>
+        Con esta compra{" "}
+        {selectedProduct?.is_redemption ? "redimiste" : "acumulaste"} :
+      </Text>
+      <Text>{selectedProduct?.points} puntos</Text>
+      <Button>Aceptar</Button>
     </Box>
   );
 };
