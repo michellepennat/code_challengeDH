06799389d1e3f443837ec3cd7a4dfc11ec10dd1b
Michelle Penna <alejandrapenna15@gmail.com>
2023-04-07 15:17:10 -0500

    refactor(DetailProduct): add styles

--- src/pages/detail-product/index.tsx
+++ src/pages/detail-product/index.tsx
@@ -1,7 +1,9 @@
 import moment from "moment";
-import { Box, Button, Image, Text } from "native-base";
-import useDetailProduct from "./hooks";
 import "moment/locale/es";
+import { Box, Button, Image, ScrollView, StatusBar, Text } from "native-base";
+import { Platform, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import useDetailProduct from "./hooks";
 
 moment.locale("es");
 
@@ -9,29 +11,110 @@ const DetailProductScreen = () => {
   const { selectedProduct } = useDetailProduct();
   // if (!selectedProduct) <>No</>;
   return (
-    <Box safeArea h="100%" justifyContent="center" alignItems="center">
-      <Text>Hola {selectedProduct?.product}</Text>
-      <Image
-        size="100px"
-        borderRadius={10}
-        alt={selectedProduct?.product}
-        source={{
-          uri: selectedProduct?.image,
-        }}
-      />
-      <Text>Detalles del producto:</Text>
-      <Text>
-        Comprado el{" "}
-        {moment(selectedProduct?.createdAt).format("DD [de] MMMM, YYYY")}
-      </Text>
-      <Text>
-        Con esta compra{" "}
-        {selectedProduct?.is_redemption ? "redimiste" : "acumulaste"} :
-      </Text>
-      <Text>{selectedProduct?.points} puntos</Text>
-      <Button>Aceptar</Button>
-    </Box>
+    <>
+      <SafeAreaView style={DetailProductStyle.areaView}>
+        <Text style={DetailProductStyle.title}>{selectedProduct?.product}</Text>
+      </SafeAreaView>
+      <ScrollView style={DetailProductStyle.wrapper}>
+        <Box
+          shadow="3"
+          style={DetailProductStyle.boxImage}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Image
+            size="200px"
+            borderRadius={10}
+            alt={selectedProduct?.product}
+            source={{
+              uri: selectedProduct?.image,
+            }}
+          />
+        </Box>
+        <Text style={DetailProductStyle.subtitle}>Detalles del producto:</Text>
+        <Text style={DetailProductStyle.textDate}>
+          Comprado el{" "}
+          {moment(selectedProduct?.createdAt).format("DD [de] MMMM, YYYY")}
+        </Text>
+        <Text style={DetailProductStyle.subtitle}>
+          Con esta compra{" "}
+          {selectedProduct?.is_redemption ? "redimiste" : "acumulaste"} :
+        </Text>
+        <Text style={DetailProductStyle.textPoints}>
+          {selectedProduct?.points} puntos
+        </Text>
+        <Button
+          _text={{ fontWeight: "bold" }}
+          style={DetailProductStyle.primaryButton}
+          width="100%"
+        >
+          Aceptar
+        </Button>
+        <StatusBar barStyle="dark-content" backgroundColor="#CFD6FF" />
+      </ScrollView>
+    </>
   );
 };
 
 export default DetailProductScreen;
+
+const DetailProductStyle = StyleSheet.create({
+  areaView: {
+    backgroundColor: "#CFD6FF",
+    padding: 20,
+    paddingBottom: Platform.OS === "ios" ? 0 : 24,
+  },
+  wrapper: {
+    backgroundColor: "#F8F8F8,",
+    display: "flex",
+    flexDirection: "column",
+    padding: 20,
+  },
+  title: {
+    // fontFamily:'Avenir',
+    fontStyle: "normal",
+    fontWeight: "800",
+    fontSize: 24,
+    lineHeight: 28,
+  },
+  boxImage: {
+    height: 350,
+    backgroundColor: "#ffffff",
+    borderRadius: 10,
+  },
+  subtitle: {
+    // fontFamily:'Avenir',
+    fontStyle: "normal",
+    fontWeight: "800",
+    fontSize: 14,
+    lineHeight: 19,
+    color: "#9B9898",
+    marginVertical: 20,
+  },
+  textDate: {
+    // fontFamily:'Avenir',
+    fontStyle: "normal",
+    fontWeight: "800",
+    fontSize: 16,
+    lineHeight: 22,
+  },
+  textPoints: {
+    // fontFamily:'Avenir',
+    fontStyle: "normal",
+    fontWeight: "800",
+    fontSize: 16,
+    lineHeight: 22,
+    marginTop: 12,
+  },
+  primaryButton: {
+    // fontFamily:'Avenir',
+    backgroundColor: "#334FFA",
+    fontStyle: "normal",
+    fontSize: 12,
+    lineHeight: 16,
+    color: "#ffffff",
+    borderRadius: 10,
+    marginTop: 47,
+    height: 50,
+  },
+});
